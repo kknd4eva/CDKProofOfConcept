e882c493604e169a0cab07a6969170821d27389c
James Matson <James.Matson@api.net.au>
2022-12-21 22:50:11 +1100

    Initial commit

---
+++ src/Cdk2/Cdk2Stack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace Cdk2
+{
+    public class Cdk2Stack : Stack
+    {
+        internal Cdk2Stack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
